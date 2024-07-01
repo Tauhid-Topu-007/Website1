f03429ca290db379be477fc684aa2bbba0c6cc90
tauhid-topu <t.topu021@gmail.com>
2024-07-02 01:40:18 +0600

    topu

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("are you human?")
+prompt("What is your name?");
+prompt("How old are you?");
+prompt("are you interested in it sector?");
+prompt("What is your institute name?");
+prompt("your department");
